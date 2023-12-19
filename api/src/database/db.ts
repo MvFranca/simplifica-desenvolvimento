b3ef6f712edc492add59b646974834b927ec4c50
guilherme-gjv <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>
2023-12-19 12:37:13 -0300

    fix: convertendo dotEnv para number

--- api/src/database/db.ts
+++ api/src/database/db.ts
@@ -1,36 +1,31 @@
-import pg from 'pg';
+import pg from "pg";
 import dotenv from "dotenv";
 
 const { Pool } = pg;
 
 dotenv.config({ path: "./.env" });
 
-
 export async function connect() {
-  console.log("criou o pool de conexão")
+  console.log("criou o pool de conexão");
 
-  if(global.connection)
-    return global.connection.connect() 
+  if (global.connection) return global.connection.connect();
 
   const db = new Pool({
     user: process.env.DB_USER_POSTGRESS,
     password: process.env.DB_PASS,
     host: process.env.DB_HOST_POSTGRESS,
-    port: process.env.DB_PORT_POSTGRESS,
+    port: parseInt(process.env.DB_PORT_POSTGRESS || "5432"),
     database: process.env.DB,
   });
 
   const client = await db.connect();
-  console.log("criou o pool de conexão")
+  console.log("criou o pool de conexão");
 
-
-  const res = await client.query("select * from questoes")
-  console.log(res.rows[0])
-  client.release()
+  const res = await client.query("select * from questoes");
+  console.log(res.rows[0]);
+  client.release();
 
   global.connection = db;
-  
-  return db.connect()
+
+  return db.connect();
 }
-
-
