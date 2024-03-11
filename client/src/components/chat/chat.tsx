3ef3fe6652ffc2ec33497a050c40878834850efb
guilherme-gjv <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>
2024-03-11 17:37:21 -0300

    adicionando redirecionamento na página que lista dúvidas

--- client/src/components/chat/chat.tsx
+++ client/src/components/chat/chat.tsx
@@ -1,94 +1,148 @@
-import styles from '../../styles/comunidade/Chat.module.css'
-import Duvida from './duvida';
+import styles from "../../styles/comunidade/Chat.module.css";
+import DuvidaCard from "./duvida";
 
 const Chat = () => {
-    return ( 
-        <div className={styles.chat}>
-
-            <section className={styles.infoDuvidas}>
-                
-                <div className={styles.infoData}>
-                    <span className={styles.data}>
-                        18/01
-                    </span>
-                    <span className={styles.borda}/>
-                </div>
-
-                <div className={styles.fullDuvidas}>
-                    <span className={styles.setaEsquerda}>
-                        <img src="icon-arrow.png" alt="seta" />
-                    </span>
-                    <div>
-                        <Duvida/>
-                        <Duvida/>
-                        <Duvida/>
-                        <Duvida/>
-                    </div>
-                    <span className={styles.setaDireita}>
-                        <img src="icon-arrow.png" alt="seta"/>
-                    </span>
-                </div>
-                
-            </section>
-
-            
-            <section className={styles.infoDuvidas}>
-                
-                <div className={styles.infoData}>
-                    <span className={styles.data}>
-                        13/01
-                    </span>
-                    <span className={styles.borda}/>
-                </div>
-
-                <div className={styles.fullDuvidas}>
-                    <span className={styles.setaEsquerda}>
-                        <img src="icon-arrow.png" alt="seta" />
-                    </span>
-                    <div>
-                        <Duvida/>
-                        <Duvida/>
-                        <Duvida/>
-                        <Duvida/>
-                    </div>
-                    <span className={styles.setaDireita}>
-                        <img src="icon-arrow.png" alt="seta"/>
-                    </span>
-                </div>
-                
-            </section>
-         
-            <section className={styles.infoDuvidas}>
-                
-                <div className={styles.infoData}>
-                    <span className={styles.data}>
-                        07/01
-                    </span>
-                    <span className={styles.borda}/>
-                </div>
-
-                <div className={styles.fullDuvidas}>
-                    <span className={styles.setaEsquerda}>
-                        <img src="icon-arrow.png" alt="seta" />
-                    </span>
-                    <div>
-                        <Duvida/>
-                        <Duvida/>
-                        <Duvida/>
-                        <Duvida/>
-                    </div>
-                    <span className={styles.setaDireita}>
-                        <img src="icon-arrow.png" alt="seta"/>
-                    </span>
-                </div>
-                
-            </section>
-
-
-            
-
+  return (
+    <div className={styles.chat}>
+      <section className={styles.infoDuvidas}>
+        <div className={styles.infoData}>
+          <span className={styles.data}>18/01</span>
+          <span className={styles.borda} />
         </div>
-     );
-}
- 
+
+        <div className={styles.fullDuvidas}>
+          <span className={styles.setaEsquerda}>
+            <img src="icon-arrow.png" alt="seta" />
+          </span>
+          <div>
+            <DuvidaCard
+              duvida={{
+                id: 1,
+                titulo: "Variáveis",
+                descricao: "tive problemas ao definir as variáveis no vscode",
+              }}
+            />
+            <DuvidaCard
+              duvida={{
+                id: 1,
+                titulo: "Variáveis",
+                descricao: "tive problemas ao definir as variáveis no vscode",
+              }}
+            />
+            <DuvidaCard
+              duvida={{
+                id: 1,
+                titulo: "Variáveis",
+                descricao: "tive problemas ao definir as variáveis no vscode",
+              }}
+            />
+            <DuvidaCard
+              duvida={{
+                id: 1,
+                titulo: "Variáveis",
+                descricao: "tive problemas ao definir as variáveis no vscode",
+              }}
+            />
+          </div>
+          <span className={styles.setaDireita}>
+            <img src="icon-arrow.png" alt="seta" />
+          </span>
+        </div>
+      </section>
+
+      <section className={styles.infoDuvidas}>
+        <div className={styles.infoData}>
+          <span className={styles.data}>13/01</span>
+          <span className={styles.borda} />
+        </div>
+
+        <div className={styles.fullDuvidas}>
+          <span className={styles.setaEsquerda}>
+            <img src="icon-arrow.png" alt="seta" />
+          </span>
+          <div>
+            <DuvidaCard
+              duvida={{
+                id: 1,
+                titulo: "Variáveis",
+                descricao: "tive problemas ao definir as variáveis no vscode",
+              }}
+            />
+            <DuvidaCard
+              duvida={{
+                id: 1,
+                titulo: "Variáveis",
+                descricao: "tive problemas ao definir as variáveis no vscode",
+              }}
+            />
+            <DuvidaCard
+              duvida={{
+                id: 1,
+                titulo: "Variáveis",
+                descricao: "tive problemas ao definir as variáveis no vscode",
+              }}
+            />
+            <DuvidaCard
+              duvida={{
+                id: 1,
+                titulo: "Variáveis",
+                descricao: "tive problemas ao definir as variáveis no vscode",
+              }}
+            />
+          </div>
+          <span className={styles.setaDireita}>
+            <img src="icon-arrow.png" alt="seta" />
+          </span>
+        </div>
+      </section>
+
+      <section className={styles.infoDuvidas}>
+        <div className={styles.infoData}>
+          <span className={styles.data}>07/01</span>
+          <span className={styles.borda} />
+        </div>
+
+        <div className={styles.fullDuvidas}>
+          <span className={styles.setaEsquerda}>
+            <img src="icon-arrow.png" alt="seta" />
+          </span>
+          <div>
+            <DuvidaCard
+              duvida={{
+                id: 1,
+                titulo: "Variáveis",
+                descricao: "tive problemas ao definir as variáveis no vscode",
+              }}
+            />
+            <DuvidaCard
+              duvida={{
+                id: 1,
+                titulo: "Variáveis",
+                descricao: "tive problemas ao definir as variáveis no vscode",
+              }}
+            />
+            <DuvidaCard
+              duvida={{
+                id: 1,
+                titulo: "Variáveis",
+                descricao: "tive problemas ao definir as variáveis no vscode",
+              }}
+            />
+            <DuvidaCard
+              duvida={{
+                id: 1,
+                titulo: "Variáveis",
+                descricao: "tive problemas ao definir as variáveis no vscode",
+              }}
+            />
+          </div>
+          <span className={styles.setaDireita}>
+            <img src="icon-arrow.png" alt="seta" />
+          </span>
+        </div>
+      </section>
+    </div>
+  );
+};
+
 export default Chat;
