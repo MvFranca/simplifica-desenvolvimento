89dfdb430b7b122c88b6bf40ab56eda767db8bcc
Marcos Vinicius <viniciusfranca09@hotmail.com>
2024-04-01 15:49:24 -0300

    feat: deixando tela de duvida responsiva

--- client/src/components/menuMobile/MenuMobile.tsx
+++ client/src/components/menuMobile/MenuMobile.tsx
@@ -11,23 +11,23 @@ const MenuMobile = () => {
     <div className={styles.container}>
       <nav>
         <NavLink to={'/'} className={styles.home} >
-          <img src="./home.png" alt="Home" />
+          <img src="/home.png" alt="Home" />
         </NavLink>
         <NavLink to={'/estudomanual'}  className={styles.home}>
-          <img src="./book.png" alt="Aprenda" />
+          <img src="/book.png" alt="Aprenda" />
         </NavLink>
         <NavLink to={'/comunidade'}  className={styles.home}>
-          <img src="./comunidade.png" alt="Comunidade" />
+          <img src="/comunidade.png" alt="Comunidade" />
         </NavLink>
         <NavLink to={'/ranking'}  className={styles.home}>
-          <img src="./ranking.png" alt="Ranking" />
+          <img src="/ranking.png" alt="Ranking" />
         </NavLink>
         <NavLink to={'/perfil'}  className={styles.home}>
         {
             img ?
             <img src={img} alt="Perfil" className={styles.myImage}/>
             :
-            <img src="./perfil.png" alt="Perfil" />
+            <img src="/perfil.png" alt="Perfil" />
         }
           </NavLink>
         {/* <a onClick={sair} >
