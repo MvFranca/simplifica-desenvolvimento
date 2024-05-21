2468c6924aeae7fb2cd4b82b4c267cfa2bfad246
guilherme-gjv <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>
2024-05-21 19:41:32 -0300

    fix: try catch no PostComentario Controller

--- api/src/controllers/community.ts
+++ api/src/controllers/community.ts
@@ -175,44 +175,55 @@ export const FindComentarioById = async (req: Request, res: Response) => {
 };
 
 export const PostComentario = async (req: Request, res: Response) => {
-  const { descricao, titulo, url_img, /* data, hora,*/ id_duvida, idUser } =
-    req.body;
+  try {
+    const { descricao, titulo, url_img, /* data, hora,*/ id_duvida, idUser } =
+      req.body;
 
-  if (!titulo) return res.status(422).json({ msg: "O título é obrigatório!" });
-  if (!descricao)
-    return res.status(422).json({ msg: "A descrição é obrigatória!" });
-  if (!id_duvida)
-    return res.status(422).json({ msg: `'id_duvida' não informado.` });
+    if (!titulo)
+      return res.status(422).json({ msg: "O título é obrigatório!" });
+    if (!descricao)
+      return res.status(422).json({ msg: "A descrição é obrigatória!" });
+    if (!id_duvida)
+      return res.status(422).json({ msg: `'id_duvida' não informado.` });
 
-  const id_duvidaInt = parseInt(id_duvida as string);
+    const id_duvidaInt = parseInt(id_duvida as string);
 
-  if (isNaN(id_duvidaInt)) {
+    if (isNaN(id_duvidaInt)) {
+      return res
+        .status(422)
+        .json({ msg: `'id_duvida' informado não é um número (NaN).` });
+    }
+
+    if (!idUser)
+      return res.status(422).json({ msg: `'idUser' não informado.` });
+
+    const idUserInt = parseInt(idUser as string);
+
+    if (isNaN(idUserInt)) {
+      return res
+        .status(422)
+        .json({ msg: `'idUser' informado não é um número (NaN).` });
+    }
+
+    const prisma = new PrismaClient();
+
+    const data = await prisma.comentario.create({
+      data: {
+        descricao,
+        img_url: url_img,
+        usuarioId: idUserInt,
+        duvidaId: id_duvidaInt,
+        titulo: titulo,
+      },
+    });
+
     return res
-      .status(422)
-      .json({ msg: `'id_duvida' informado não é um número (NaN).` });
+      .status(200)
+      .json({ msg: "Comentário enviado com sucesso!", data });
+  } catch (error) {
+    return res.status(500).json({
+      msg: "Servidor indisponível.",
+      error,
+    });
   }
-
-  if (!idUser) return res.status(422).json({ msg: `'idUser' não informado.` });
-
-  const idUserInt = parseInt(idUser as string);
-
-  if (isNaN(idUserInt)) {
-    return res
-      .status(422)
-      .json({ msg: `'idUser' informado não é um número (NaN).` });
-  }
-
-  const prisma = new PrismaClient();
-
-  const data = await prisma.comentario.create({
-    data: {
-      descricao,
-      img_url: url_img,
-      usuarioId: idUserInt,
-      duvidaId: id_duvidaInt,
-      titulo: titulo,
-    },
-  });
-
-  return res.status(200).json({ msg: "Comentário enviado com sucesso!", data });
 };
