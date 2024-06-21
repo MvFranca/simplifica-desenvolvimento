6fc0ffaf818bbe5a1856a3e7fa02732f543e5a9e
Marcos Vinicius <viniciusfranca09@hotmail.com>
2024-06-20 23:51:30 -0300

    feat: criando rota de busca para as questões de ordenação

--- api/src/controllers/content.ts
+++ api/src/controllers/content.ts
@@ -50,6 +50,14 @@ export const getQuestions = async (req: Request, res: Response) => {
     const {id} = req.params;
     const idUserInt = parseInt(id as string);
 
+   
+    if (isNaN(idUserInt)) {
+      return res
+        .status(422)
+        .json({ msg: `'id' informado não é um número (NaN).` });
+    }
+
+
     const prisma = new PrismaClient();
 
     const data = await prisma.questao.findMany({
@@ -81,5 +89,44 @@ export const getQuestions = async (req: Request, res: Response) => {
   }
 
 
+
+}
+
+export const getOrderedQuestions = async (req: Request, res: Response) => {
+
+  try {
+    
+    const {id} = req.params
+    const idUserInt = parseInt(id as string);
+       
+    if (isNaN(idUserInt)) {
+      return res
+        .status(422)
+        .json({ msg: `'id' informado não é um número (NaN).` });
+    }
+
+    const prisma = new PrismaClient();
+
+    const dataFull = await prisma.questao.findMany({
+      where: {
+          id_grupo: idUserInt
+        },
+        include: {
+          resposta_ordenada: true
+        }
+    });
+
+    const data = dataFull.filter(item => item.resposta_ordenada.length > 0);
+
+    return res.status(200).json({
+      data,
+    });
+    
+  } catch (error) {
+    return res.status(500).json({
+      msg: "Servidor indisponível.",
+      error,
+    });
+  }
 
 }
--- api/src/routes/content.ts
+++ api/src/routes/content.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import {
     getContent,
-    getQuestions
+    getQuestions,
+    getOrderedQuestions
 } from "../controllers/content";
 
 const router = express.Router();
 
 router.get("/conteudo/:id", getContent);
 
+
 router.get("/questao/:id", getQuestions);
+
+router.get("/ordenadas/:id", getOrderedQuestions);
 
 export default router;
