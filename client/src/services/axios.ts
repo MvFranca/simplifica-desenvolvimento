e4ca362d555f8e985d9636f75965453de72ba84c
guilherme-gjv <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>
2024-06-25 23:41:31 -0300

    fix: using vite .env

--- client/src/services/axios.ts
+++ client/src/services/axios.ts
@@ -1,10 +1,8 @@
-import axios, { AxiosInstance } from "axios";
-import dotenv from "dotenv";
-dotenv.config();
+import axios, { AxiosInstance } from 'axios';
 
 export function getAPIClient(): AxiosInstance {
   const api = axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: import.meta.env.BASE_URL,
   });
 
   return api;
