6b5ab0f33a732e3ef0a8fa5c8e10e7805fadfa41
Marcos Vinicius <viniciusfranca09@hotmail.com>
2024-07-01 21:33:57 -0300

    package em teste

--- client/src/services/axios.ts
+++ client/src/services/axios.ts
@@ -2,7 +2,8 @@ import axios, { AxiosInstance } from 'axios';
 
 export function getAPIClient(): AxiosInstance {
   const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
+    // baseURL: import.meta.env.VITE_BASE_URL,
+    baseURL: "http://localhost:8000/",
   });
 
   return api;
