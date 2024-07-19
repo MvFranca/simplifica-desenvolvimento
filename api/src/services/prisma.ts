240268447d16e0cb731bfb670c10270e34c6db27
João Pedro <nmjoaopedro22@gmail.com>
2024-07-18 22:33:55 -0300

    fix: utiliza singleton para reusar as instâncias do prisma

--- api/src/services/prisma.ts
+++ api/src/services/prisma.ts
@@ -1,3 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+// Utiliza o globalThis para reutilizar a instância do prisma sempre que possível
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
