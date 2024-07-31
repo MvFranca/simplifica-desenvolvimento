6944e305b90e8d5a403cbd3ea0d294256b09bccc
Marcos Vinicius <viniciusfranca09@hotmail.com>
2024-07-30 21:16:01 -0300

    fix: permitindo acesso ao back-end

--- client/src/services/axios.ts
+++ client/src/services/axios.ts
@@ -2,8 +2,8 @@ import axios, { AxiosInstance } from 'axios';
 
 export function getAPIClient(): AxiosInstance {
   const api = axios.create({
-    // baseURL: import.meta.env.VITE_BASE_URL,
-    baseURL: "http://localhost:8000/api/",
+    baseURL: import.meta.env.VITE_BASE_URL,
+    // baseURL: "http://localhost:8000/api/",
   });
 
   return api;
